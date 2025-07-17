8cbd3f27da2e091b6674b525ea2c2d5bb35326aa
omarshaarawi <omar25shaarawi@gmail.com>
2025-07-16 20:10:20 -0500

    fix readme and auto-create db

--- shell/hindsight.zsh
+++ shell/hindsight.zsh
@@ -1,13 +1,17 @@
 export HINDSIGHT_SESSION=$(uuidgen)
+export HINDSIGHT_MODE=${HINDSIGHT_MODE:-global}
 
 zle -N hindsight-widget
 function hindsight-widget() {
   local selected
-  selected=$(hindsight --mode "${HINDSIGHT_MODE:-global}")
+  selected=$(hindsight --mode "$HINDSIGHT_MODE")
   
-  if [[ -n "$selected" ]]; then
-    BUFFER="$selected"
+  if [[ "$selected" == "__HINDSIGHT_EDIT__"* ]]; then
+    BUFFER="${selected#__HINDSIGHT_EDIT__}"
     CURSOR=${#BUFFER}
+  elif [[ -n "$selected" ]]; then
+    BUFFER="$selected"
+    zle accept-line
   fi
 }
 
